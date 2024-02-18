1581990b97b648a68312bcefef03dd06aa77971c
yingsheng01 <yingsheng_fam@hotmail.com>
2024-02-18 15:41:54 +0800

    done nav bar (except for onclick changes)

--- components/sidebar.js
+++ components/sidebar.js
@@ -7,34 +7,37 @@ class Sidebar extends HTMLElement {
       this.innerHTML = `
         <div class="sidebar">
             <img class="sidebar-logo" src="/resources/images/securehold.png" alt="IMG">
-            <nav>
-                <ul class="sidebar-nav">
-                    <li>
-                        <a href="dashboard.html">
-                            <i class="fa-solid fa-layer-group"></i>
-                            <span>Dashboard</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="fa-solid fa-chart-line"></i>
-                            <span>Data Analytics</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="annoucement.html">
-                            <i class="fa-solid fa-bell"></i>
-                            <span>Announcements</span>
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#">
-                            <i class="fa-solid fa-database"></i>
-                            <span>Database</span>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
+        
+                <div class= sidebar-nav-container>
+                    <ul class="sidebar-nav-list">
+                        <li class="sidebar-nav-li">
+                            <div>
+                                <a href="dashboard.html">
+                                    <i class="fa-solid fa-layer-group", style="padding:5px 5px 5px 10px"></i>
+                                    <span style="padding-right: 10px;">Dashboard</span>
+                                </a>
+                            </div>
+                        </li>
+                        <li class="sidebar-nav-li">
+                            <a href="#">
+                                <i class="fa-solid fa-chart-line", style="padding:10px"></i>
+                                <span style="padding-right: 10px;">Data Analytics</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-nav-li">
+                            <a href="annoucement.html">
+                                <i class="fa-solid fa-bell", style="padding:10px"></i>
+                                <span style="padding-right: 10px;">Announcements</span>
+                            </a>
+                        </li>
+                        <li class="sidebar-nav-li">
+                            <a href="#">
+                                <i class="fa-solid fa-database", style="padding:10px"></i>
+                                <span style="padding-right: 10px;">Database</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
         </div>
       `;
     }
