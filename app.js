88ceb9ac5d02ae3e3e001a7d8caaaa687282c955
dtcz <dylan.tancz@gmail.com>
2024-02-19 15:43:36 +0800

    session

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//good fucking lord do not touch this variable
 const apiLink = "http://localhost:3000/"
 
 //changing pages
@@ -8,3 +9,30 @@ function pageRouting(link) {
 // function showPopup(id, toShow) {
 //     document.getElementById(id).style.visibility = toShow ? 'visible' : 'hidden'
 // }
+
+// function hasSession() {
+//     return new Promise((resolve, reject) => {
+//         axios.get(apiLink +"checkToken", {withCredentials: true})
+//         .then((resp) => {
+//             resolve(resp.data.status == 200);
+//         })
+//         .catch((error) => {
+//             console.log("API Error: " + error);
+//             reject(false);
+//         });
+//     })
+// }
+
+function hasSession(goDashboard) {
+    axios.get(apiLink +"checkToken", {withCredentials: true})
+    .then((resp) => {
+        if (resp.data.status == 200) {
+            return pageRouting( goDashboard ? "pages/dashboard.html" : "")
+        }
+        return pageRouting("/pages/login.html")
+    })
+    .catch((error) => {
+        console.log("API Error: " + error);
+        return pageRouting("/pages/login.html")
+    });
+}
