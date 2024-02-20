594bda2ffac10c64d11d3d81952fb5f0dbde9a8d
dtcz <dylan.tancz@gmail.com>
2024-02-21 03:29:16 +0800

    jesus how many <a> buttons are there

--- components/header.js
+++ components/header.js
@@ -1,30 +1,50 @@
 class Header extends HTMLElement {
     constructor() {
       super();
+	  this.headerTitle = ""
+	  this.isProfileOpen = false
     }
   
     connectedCallback() {
-      this.innerHTML = `
-        <header class="header">
-            <div class="content-title">
-                <span>Dashboard</span>
-            </div>
+		this.headerTitle = this.getAttribute("headerTitle") || 'Securehold'
 
-            <div class="profile">
-                <button id="profileButton">
-                    <i class="fa-solid fa-user"></i>
-                    <span>nigga</span>
-                </button>
-            </div>
+      	this.innerHTML = `
+			<div class="header">
+				<div class="header-title">
+					<span style="text-align: right;">${this.headerTitle}</span>
+				</div>
 
-            <ul id="profileDropdown" class="profile-dropdown">
-                <li><a href="#">Edit Profile</a></li>
-                <li><a href="change_password.html">Change Password</a></li>
-                <li><a href="#">Logout</a></li>
-            </ul>
-        </header>
-      `;
+				<div class="profile-button-container">
+					<button class="profile-button" id="profileButton">
+						<i class="fa-solid fa-user"></i>
+						<span style="padding: 7px;">Profile</span>
+					</button>
+
+
+					<div id="profileDropdown" class="profile-dropdown">
+						<div class="dropdown-item" onclick="pageRouting('profile.html')">Edit Profile</div>
+						<div class="dropdown-item" onclick="pageRouting('change_password.html')">Change Password</div>
+						<div class="dropdown-item">Logout</div>
+					</div>
+				<div>
+			</div>
+      	`;
+
+		document.addEventListener("click", (event) => {
+			const profileButton = document.getElementById("profileButton");
+			const profileDropdown = document.getElementById("profileDropdown");
+			if (profileButton.contains(event.target)) {
+				this.isProfileOpen = !this.isProfileOpen
+				profileDropdown.style.visibility = this.isProfileOpen ? 'visible': 'hidden'
+			}
+			else if (this.isProfileOpen && !profileDropdown.contains(event.target)) {
+				this.isProfileOpen = false
+				profileDropdown.style.visibility = 'hidden'
+			}
+		});
     }
+
+	
   }
 
   customElements.define('header-component', Header);
