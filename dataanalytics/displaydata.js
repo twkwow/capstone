a15dfa29d8b489b1ea2cd5cf0a25a0cfb1cf7b6a
twkwow <wenkanglol444@gmail.com>
2024-03-13 05:42:55 +0800

    dataanalytics update

---
+++ dataanalytics/displaydata.js
@@ -0,0 +1,105 @@
+//how many assigned locker compartment per locker location for example 2/25
+const today = new Date();
+
+const threeDaysAgo = new Date(today);
+threeDaysAgo.setDate(today.getDate() - 3);
+const twoDaysAgo = new Date(today);
+twoDaysAgo.setDate(today.getDate() - 2);
+const oneDayAgo = new Date(today);
+oneDayAgo.setDate(today.getDate() - 1);
+
+const formatDate = date => {
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const year = date.getFullYear();
+  return `${month}/${day}/${year}`;
+};
+
+const xArray = [
+  formatDate(threeDaysAgo),
+  formatDate(twoDaysAgo),
+  formatDate(oneDayAgo),
+  formatDate(today),
+  "Tomorrow"
+];
+
+const yArray = [3, 2, 10, 5, 15];
+
+const colors = yArray.map((value, index) => {
+  if (index === yArray.length - 1) {
+    return "red";
+  } else {
+    return "rgba(0,0,255,0.6)";
+  }
+});
+
+const data = [{
+  x: xArray,
+  y: yArray,
+  type: "bar",
+  orientation: "v",
+  marker: { color: colors }
+}];
+
+const layout = {
+  width: 1000,
+  font: {
+    family: 'Tahoma',
+    size: 18
+  }
+};
+
+Plotly.newPlot("myPlotBar", data, layout);
+
+
+//LINE CHART open_count against last_used
+const xArrayud = ["00:00", "04:00", "08:00", "12:00", "16:00", "20:00", "23:59"]
+const yArrayud = [1, 5, 7, 2, 5, 6, 10];
+
+const dataud = [{
+  x: xArrayud,
+  y: yArrayud,
+  mode: "lines",
+  type: "scatter"
+}];
+
+const layoutud = {
+  xaxis: {
+    type: 'time',
+    title: "Time Of The Day"
+  },
+  yaxis: {
+    title: "Locker Open Count"
+  },
+  width: 1000,
+  font: {
+    family: 'Tahoma',
+    size: 18
+  }
+};
+
+Plotly.newPlot("myPlotLine", dataud, layoutud);
+
+//pie CHART usage_minutes against location
+
+const xArraypie = ["Taylors Homeless Locker", "Pertiwi Soup Kitchen", "SS15", "Chow Kit"]
+const yArraypie = [40, 50, 110, 20];
+
+const datapie = [{
+  labels: xArraypie,
+  values: yArraypie,
+  type: "pie",
+  hole:.3
+}];
+
+const layoutpie = {
+  title: "Locker Usage Duration (in minutes)",
+  width:1000,
+  font: {
+      family: 'Tahoma',
+      size: 18
+  }
+
+};
+
+Plotly.newPlot("myPlotPie", datapie, layoutpie);
