15a66dde72d1319e249f59868beaece541673c73
dtcz <dylan.tancz@gmail.com>
2024-03-13 01:25:00 +0800

    le fix

--- components/sidebar.js
+++ components/sidebar.js
@@ -7,54 +7,51 @@ class Sidebar extends HTMLElement {
       this.innerHTML = `
         <div class="sidebar">
             <img class="sidebar-logo" src="/resources/images/securehold.png" alt="IMG">
-        
-                <div class= sidebar-nav-container>
-                    <ul class="sidebar-nav-list">
-                        <li class="sidebar-nav-list-li">
-                            <a href="dashboard.html">
-                                <button class="sidebar-nav-btn">
-                                    <i class="fa-solid fa-layer-group", style="padding:7px"></i>
-                                    <span style="padding: 7px;">Dashboard</span>
-                                </button>
-                            </a>
-                        </li>
-                        <li class="sidebar-nav-list-li">
-                            <a href="analytics.html">
-                                <button class="sidebar-nav-btn">
-                                    <i class="fa-solid fa-chart-line", style="padding:7px"></i>
-                                    <span style="padding: 7px;">Data Analytics</span>
-                                </button>
-                            </a>
-                        </li>
-                        <li class="sidebar-nav-list-li">
-                            <a href="annoucement.html">
-                                <button class="sidebar-nav-btn">
-                                    <i class="fa-solid fa-bell", style="padding:7px"></i>
-                                    <span style="padding: 7px;">Announcements</span>
-                                </button>
-                            </a>
-                        </li>
-                        <li class="sidebar-nav-list-li">
-                            <a href="usernotification.html">
-                                <button class="sidebar-nav-btn">
-                                    <i class="fa-solid fa-circle-exclamation", style="padding:7px"></i>
-                                    <span style="padding: 7px;">User Notifications</span>
-                                </button>
-                            </a>
-                        </li>
-                        <li class="sidebar-nav-list-li">
-                            <a href="database.html">
-                                <button class="sidebar-nav-btn">
-                                    <i class="fa-solid fa-database", style="padding:7px"></i>
-                                    <span style="padding: 7px;">Database</span>
-                                </button>
-                            </a>
-                        </li>
-                        <li class="footer-text-lol">
-                            <span>Powered by Group 3 Final Boss<span>
-                        </li>
-                    </ul>
-                </div>
+                <ul class="sidebar-nav-list">
+                    <li class="sidebar-nav-list-li">
+                        <a href="dashboard.html">
+                            <button class="sidebar-nav-btn">
+                                <i class="fa-solid fa-layer-group", style="padding:7px"></i>
+                                <span style="padding: 7px;">Dashboard</span>
+                            </button>
+                        </a>
+                    </li>
+                    <li class="sidebar-nav-list-li">
+                        <a href="analytics.html">
+                            <button class="sidebar-nav-btn">
+                                <i class="fa-solid fa-chart-line", style="padding:7px"></i>
+                                <span style="padding: 7px;">Data Analytics</span>
+                            </button>
+                        </a>
+                    </li>
+                    <li class="sidebar-nav-list-li">
+                        <a href="annoucement.html">
+                            <button class="sidebar-nav-btn">
+                                <i class="fa-solid fa-bell", style="padding:7px"></i>
+                                <span style="padding: 7px;">Announcements</span>
+                            </button>
+                        </a>
+                    </li>
+                    <li class="sidebar-nav-list-li">
+                        <a href="usernotification.html">
+                            <button class="sidebar-nav-btn">
+                                <i class="fa-solid fa-circle-exclamation", style="padding:7px"></i>
+                                <span style="padding: 7px;">User Notifications</span>
+                            </button>
+                        </a>
+                    </li>
+                    <li class="sidebar-nav-list-li">
+                        <a href="database.html">
+                            <button class="sidebar-nav-btn">
+                                <i class="fa-solid fa-database", style="padding:7px"></i>
+                                <span style="padding: 7px;">Database</span>
+                            </button>
+                        </a>
+                    </li>
+                    <li class="footer-text-lol">
+                        <span>Powered by Group 3 Final Boss<span>
+                    </li>
+                </ul>
         </div>
       `;
     }
